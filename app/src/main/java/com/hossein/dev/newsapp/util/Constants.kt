45ef6f038886958dbcf33e586e080973b57de20a
hossein <s.hossein468@gmail.com>
2025-02-06 19:17:02 +0330

    feat: add sizes for compose UI

--- app/src/main/java/com/hossein/dev/newsapp/util/Constants.kt
+++ app/src/main/java/com/hossein/dev/newsapp/util/Constants.kt
@@ -1,7 +1,17 @@
 package com.hossein.dev.newsapp.util
 
+import androidx.compose.ui.unit.dp
+
 object Constants {
 
     const val USER_SETTINGS_KEY = "userSettings"
     const val APP_ENTRY_KEY = "appEntry"
 }
+
+val tinySize = 4.dp
+val smallSize = 8.dp
+val semiSmallSize = 12.dp
+val defaultSize = 16.dp
+val mediumSize = 24.dp
+val semiLargeSize = 32.dp
+val largeSize = 48.dp
