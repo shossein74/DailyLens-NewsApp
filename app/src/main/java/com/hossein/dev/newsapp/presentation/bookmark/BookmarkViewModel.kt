3b4360d72822b7c166cd9f3d0b902f8f2bd8f205
hossein <s.hossein468@gmail.com>
2025-02-06 19:29:57 +0330

    feat: add empty bookmark view model

---
+++ app/src/main/java/com/hossein/dev/newsapp/presentation/bookmark/BookmarkViewModel.kt
@@ -0,0 +1,8 @@
+package com.hossein.dev.newsapp.presentation.bookmark
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+//@HiltViewModel
+class BookmarkViewModel: ViewModel() {
+}
