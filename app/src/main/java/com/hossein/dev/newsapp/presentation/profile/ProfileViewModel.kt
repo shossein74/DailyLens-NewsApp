7d6327157006b441c4026eb046561bb093b11607
hossein <s.hossein468@gmail.com>
2025-02-06 19:29:37 +0330

    feat: add empty profile view model

---
+++ app/src/main/java/com/hossein/dev/newsapp/presentation/profile/ProfileViewModel.kt
@@ -0,0 +1,8 @@
+package com.hossein.dev.newsapp.presentation.profile
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+//@HiltViewModel
+class ProfileViewModel : ViewModel() {
+}
